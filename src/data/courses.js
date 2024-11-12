0d6c7a521d93f28e6a4a7b3a13de3066a47f5243
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-11-12 15:01:42 -0500

    Add: dummy courses data

---
+++ src/data/courses.js
@@ -0,0 +1,19 @@
+const courses = [
+  {
+    id: 1,
+    title: "Diseño y Desarrollo Web",
+    slug: "diseno-desarrollo-web",
+  },
+  {
+    id: 2,
+    title: "Introducción a la programación",
+    slug: "introduccion-a-la-programacion",
+  },
+  {
+    id: 3,
+    title: "Diseño y Programación de base de datos",
+    slug: "diseno-y-programación-de-base-de-datos",
+  },
+];
+
+export default courses;
