33b85545d4a3019ede027000fae5caa1f69dbf60
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-11-12 14:28:53 -0500

    Add: Load env variables

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+process.loadEnvFile();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,6 +11,8 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello world!" });
 });
 
-app.listen(2024, () => {
-  console.log(`Server is up and running on http://localhost:2024`);
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is up and running on http://localhost:${process.env.PORT}`
+  );
 });
